e923084f316c4457c00d575b39188e65bb698191
matiar <110628257+Matiar15@users.noreply.github.com>
2025-12-14 14:08:25 +0100

    Add `connection_time` utility for measuring MongoDB connection performance

---
+++ src/metrics/connection_time.py
@@ -0,0 +1,20 @@
+import logging
+
+import pymongo
+
+from src.utils.catch_time import catch_time
+
+_logger = logging.getLogger(__name__)
+
+def connection_time(n: int, client: pymongo.MongoClient,) -> tuple[float, list[float]]:
+    client.close()
+    collected = []
+
+    with catch_time() as t_1:
+        for i in range(n):
+            with catch_time() as t_2:
+                client._connect()
+                client.close()
+                collected.append(t_2())
+
+    return n / t_1(), collected
